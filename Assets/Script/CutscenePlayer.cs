9cb487efef2ed18a73c7f4bdb6fa57599a6b51a0
DungLuong1602 <ITCSIU24024@student.hcmiu.edu.vn>
2025-09-23 03:45:08 +0700

    new update

---
+++ Assets/Script/CutscenePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+
+public class CutscenePlayer : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public CanvasGroup canvasGroup;
+    public string nextSceneName = "Phòng ngủ"; // đổi tên scene thật của bạn
+
+    private bool isFading = true;
+    private float fadeSpeed = 1f;
+
+    void Start()
+    {
+        // Bắt đầu play video
+        videoPlayer.Play();
+
+        // Khi video kết thúc → load scene mới
+        videoPlayer.loopPointReached += OnVideoFinished;
+
+        // Bắt đầu với màn hình đen
+        canvasGroup.alpha = 0;
+    }
+
+    void Update()
+    {
+        if (isFading && canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime * fadeSpeed;
+        }
+    }
+
+    void OnVideoFinished(VideoPlayer vp)
+    {
+        // Khi kết thúc video → load scene
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
+
+
---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Rigidbody rb;
+    public Animator animator;
+    public bool isRunning;
+
+
+}
