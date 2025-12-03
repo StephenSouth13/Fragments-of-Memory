fc5823643606204733de6402e36461cba558f5fb
DungLuong1602 <ITCSIU24024@student.hcmiu.edu.vn>
2025-12-03 20:26:50 +0700

    update, main menu, sound click, Cutscene

---
+++ V-Unique/Assets/Script/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Cái loa để phát tiếng")]
+    public AudioSource sfxSource;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject); 
+        }
+        else
+        {
+
+            Destroy(gameObject);
+        }
+    }
+    
+    public void PlaySFX(AudioClip clip)
+    {
+        if (clip != null && sfxSource != null)
+        {
+            sfxSource.PlayOneShot(clip);
+        }
+    }
+}
